c7ef92a78d93710de993b23f557ec23e7f9766c4
brunomoraesferreira <89628890+brunomoraesferreira@users.noreply.github.com>
2022-04-13 21:33:39 -0300

    130422.2133

--- js/sanfona.js
+++ js/sanfona.js
@@ -1,14 +1,14 @@
 // SANFONA
-var acc = document.getElementsByClassName("lbl-sc");
+var lbl = document.getElementsByClassName("lbl-sc");
 var i;
 var j;
-for (i = 0; i < acc.length; i++) 
+for (i = 0; i < lbl.length; i++) 
 {
-  acc[i].addEventListener("click", function() 
+  lbl[i].addEventListener("click", function() 
   {
-    for (j = 0; j < acc.length; j++) 
+    for (j = 0; j < lbl.length; j++) 
     {
-      acc[j].classList.remove('sec-ativa');
+      lbl[j].classList.remove('sec-ativa');
     }
     this.classList.toggle("sec-ativa");
     //===================================
