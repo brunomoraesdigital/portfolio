6be8b555ba510faf5940a1d9e694b67f1a35b014
Bruno Moraes <89628890+brunomoraesdigital@users.noreply.github.com>
2023-02-26 14:23:39 -0300

    loader

--- public/js/precarregamento.js
+++ public/js/precarregamento.js
@@ -1,6 +1,29 @@
         // LOADER
-        const sobreposicao = document.querySelector("#precarregamento");
+      /*  const sobreposicao = document.querySelector("#precarregamento");
         window.addEventListener("load", function () {
           sobreposicao.style.display = "none";
           //sobreposicao.style.background = "skyblue";
-        })
+        })*/
+
+/*... - pontos para o texto carregando ideias*/
+
+function updateDots() {
+  const dots = document.getElementById("dots");
+  if (dots.textContent.length === 3) {
+    dots.textContent = "";
+  } else {
+    dots.textContent += ".";
+  }
+}
+let intervalId = setInterval(updateDots, 333);
+
+/*loader*/
+
+function esconderSobreposicao() {
+  // LOADER
+  const sobreposicao = document.querySelector("#precarregamento");
+  window.addEventListener("load", function () {
+    sobreposicao.style.display = "none";
+  })
+}
+esconderSobreposicao();
