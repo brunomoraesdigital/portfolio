154afb63b863db7a24da13ee943df7d6211cdf88
Bruno Moraes <brunomoraes.dev@gmail.com>
2025-08-25 20:14:13 -0300

    250825.2014

--- public/js/copy.js
+++ public/js/copy.js
@@ -7,4 +7,4 @@ const anoAtual = new Date().getFullYear();
 // Atualiza o conteúdo do elemento com o ano atual
 anoAtualElement.innerHTML = `<a href="https://bmfolio.web.app/" target="_blank" rel="noopener noreferrer">
     © ${anoAtual} Bruno Moraes - Evoluindo a cada código
-</a> | Licença AGPL v3`;
+</a>&nbsp;| Licença AGPL v3`;
