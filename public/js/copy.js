4fce784ea7676fc928a9052b62b76bf28f359d92
Bruno Moraes <brunomoraes.dev@gmail.com>
2025-08-27 18:41:50 -0300

    270825

--- public/js/copy.js
+++ public/js/copy.js
@@ -5,5 +5,5 @@ const anoAtualElement = document.getElementById("ano-atual");
 const anoAtual = new Date().getFullYear();
 
 // Atualiza o conteúdo do elemento com o ano atual
-anoAtualElement.innerHTML = `<a href="https://bmfolio.web.app/" target="_blank" rel="noopener noreferrer">
-    © ${anoAtual} Bruno Moraes</a>&nbsp;|&nbsp;<a href="licenca-agplv3.html" title="Visualizar licença AGPLv3" target="_blank">Evoluindo a cada código</a>`;
+anoAtualElement.innerHTML = `<a href="licenca-agplv3.html" title="Visualizar licença AGPLv3" target="_blank" rel="noopener noreferrer">
+    © ${anoAtual} Bruno Moraes &nbsp;|&nbsp;AGPL-3.0 license</a>`;
